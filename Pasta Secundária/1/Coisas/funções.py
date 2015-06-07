c342bc2b881cb5559633317c457859c3b4cee2c7
Carlos <carlosmacedo025@gmail.com>
2015-06-07 20:01:58 -0300

    Delete de pastas desnecessárias
    Teste

--- Pasta Secundária/1/Coisas/funções.py
+++
@@ -1,8 +0,0 @@
-import os, pygame
-from pygame.locals import *
-def loadImage( nome ):
-    ''' Carregar Imagens '''
-    nome = os.path.join('image', nome)
-    imagem = pygame.image.load( nome ).convert_alpha()
-
-    return imagem, imagem.get_rect()
